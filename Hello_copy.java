005db19af06d24ba52abe39ba3c9c904dd12b561
hemanthkuna197 <hemanthkuna197@gmail.com>
2026-03-30 14:58:27 +0530

    new commit

--- Hello_copy.java
+++ Hello_copy.java
@@ -2,5 +2,8 @@ public class Hello_copy {
     public static void main(String args[]){
         System.out.println("hello world");
         System.out.println("new Hello world");
+        int a=0;
+        int b=20;
+        System.out.println("sum of a and b: "+(a+b));
     }
 }
